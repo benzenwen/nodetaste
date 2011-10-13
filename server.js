40faf84976ddd824766c33620152518ffc6adcfe
Ben Wen <github@benwen.com>
2011-10-13 01:50:00 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+// ** Note: Wed Oct 12 16:23:53 2011 **
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
